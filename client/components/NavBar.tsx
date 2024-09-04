915dbfec4e2f493b48f03e8a47fdd803be293a0d
Georgia Harris <ggjune26@gmail.com>
2024-09-04 15:15:53 +1200

    Added the Authenticated.tsx functions to the NavBar to being handling signing in and out

---
+++ client/components/NavBar.tsx
@@ -0,0 +1,37 @@
+import { IfAuthenticated, IfNotAuthenticated } from './Authenticated.tsx'
+import { useAuth0 } from '@auth0/auth0-react'
+
+function NavBar() {
+  const { loginWithRedirect, logout, user } = useAuth0()
+
+  const handleSignOut = () => {
+    logout()
+  }
+
+  const handleSignIn = () => {
+    loginWithRedirect()
+  }
+
+  return (
+    <>
+      <IfAuthenticated>
+        <button onClick={handleSignOut}>Sign out</button>
+        {user && (
+          <div>
+            <img src={user?.picture} alt="profile pic" />
+            <p>
+              Signed in as: {user?.nickname} / {user?.given_name}
+            </p>
+            <p>Email: {user?.email}</p>
+          </div>
+        )}
+      </IfAuthenticated>
+      <IfNotAuthenticated>
+        <button onClick={handleSignIn}>Sign in</button>
+      </IfNotAuthenticated>
+      <h1>Fruit FTW!</h1>
+    </>
+  )
+}
+
+export default NavBar
